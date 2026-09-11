217851e7913eed5459b354c112e1e455797c0b1e
Javier Pepe <javierpepe@gmail.com>
2026-09-11 12:43:17 -0300

    itegra plu

--- odoo-custom-addons/product_itegra/models/product.py
+++ odoo-custom-addons/product_itegra/models/product.py
@@ -1,30 +1,49 @@
-from odoo import models, fields, api
-from datetime import datetime, timedelta
-from odoo.exceptions import UserError
+import base64
+import logging
 from io import StringIO
 
-import logging
+from odoo import models, fields, api
+from odoo.exceptions import UserError
+
 _logger = logging.getLogger(__name__)
+
+
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     def pricelist_itegra(self):
-        fp = open('/mnt/extra-addons/laamistad/odoo-custom-addons/product_itegra/static/CODIGOS_PLU_ODOO.TXT','w')
+        # 1. Generar el contenido en memoria
+        buffer = StringIO()
         for rec in self.env['product.template'].search([]):
             try:
                 if rec.default_code and rec.barcode:
-                    if rec.barcode[0] == '2':
-                        t='P'
-                    else:
-                        t='N'
+                    t = 'P' if rec.barcode[0] == '2' else 'N'
                     if float(rec.list_price) > 0:
-                        fp.write('%05d%06d%-26s%07.1f%s\n' % (int(rec.default_code),int(rec.default_code),rec.name[:25],float(rec.list_price),t) )
-            except:
+                        buffer.write('%05d%06d%-26s%07.1f%s\n' % (
+                            int(rec.default_code),
+                            int(rec.default_code),
+                            rec.name[:25],
+                            float(rec.list_price),
+                            t,
+                        ))
+            except Exception as e:
+                _logger.warning("Error procesando producto %s: %s", rec.id, e)
                 continue
-        fp.close()
+
+        file_content = buffer.getvalue()
+        buffer.close()
+
+        # 2. Crear un attachment con el contenido (en base64)
+        attachment = self.env['ir.attachment'].create({
+            'name': 'CODIGOS_PLU_ODOO.TXT',
+            'type': 'binary',
+            'datas': base64.b64encode(file_content.encode('utf-8')),
+            'mimetype': 'text/plain',
+        })
+
+        # 3. Devolver acción URL al endpoint de descarga de Odoo
         return {
-                'type': 'ir.actions.act_url',
-                'url': 'product_itegra/static/CODIGOS_PLU_ODOO.TXT',
-                'context': self._context, 
-                'target': 'self',
+            'type': 'ir.actions.act_url',
+            'url': '/web/content/%s?download=true' % attachment.id,
+            'target': 'self',
         }
